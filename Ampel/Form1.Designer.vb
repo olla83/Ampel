3f6242a413219359c62894a7c7e7d119e364dc4a
Oliver Lang <oliver.lang83@gmail.com>
2019-08-08 16:24:40 +0200

    Ampel fertig

--- Ampel/Form1.Designer.vb
+++ Ampel/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frmAmpel
     Inherits System.Windows.Forms.Form
 
     'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
@@ -22,10 +22,71 @@ Partial Class Form1
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.lblRot = New System.Windows.Forms.Label()
+        Me.lblgelb = New System.Windows.Forms.Label()
+        Me.lblGrün = New System.Windows.Forms.Label()
+        Me.btnWeiter = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblRot
+        '
+        Me.lblRot.BackColor = System.Drawing.Color.Gray
+        Me.lblRot.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblRot.Location = New System.Drawing.Point(301, 18)
+        Me.lblRot.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblRot.Name = "lblRot"
+        Me.lblRot.Size = New System.Drawing.Size(110, 64)
+        Me.lblRot.TabIndex = 0
+        '
+        'lblgelb
+        '
+        Me.lblgelb.BackColor = System.Drawing.Color.Gray
+        Me.lblgelb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblgelb.Location = New System.Drawing.Point(301, 130)
+        Me.lblgelb.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblgelb.Name = "lblgelb"
+        Me.lblgelb.Size = New System.Drawing.Size(110, 64)
+        Me.lblgelb.TabIndex = 1
+        '
+        'lblGrün
+        '
+        Me.lblGrün.BackColor = System.Drawing.Color.Gray
+        Me.lblGrün.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblGrün.Location = New System.Drawing.Point(301, 263)
+        Me.lblGrün.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblGrün.Name = "lblGrün"
+        Me.lblGrün.Size = New System.Drawing.Size(110, 64)
+        Me.lblGrün.TabIndex = 2
+        '
+        'btnWeiter
+        '
+        Me.btnWeiter.Location = New System.Drawing.Point(301, 373)
+        Me.btnWeiter.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnWeiter.Name = "btnWeiter"
+        Me.btnWeiter.Size = New System.Drawing.Size(111, 64)
+        Me.btnWeiter.TabIndex = 3
+        Me.btnWeiter.Tag = "0"
+        Me.btnWeiter.Text = "Start"
+        Me.btnWeiter.UseVisualStyleBackColor = True
+        '
+        'frmAmpel
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.Controls.Add(Me.btnWeiter)
+        Me.Controls.Add(Me.lblGrün)
+        Me.Controls.Add(Me.lblgelb)
+        Me.Controls.Add(Me.lblRot)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Name = "frmAmpel"
+        Me.Text = "Ampel"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents lblRot As Label
+    Friend WithEvents lblgelb As Label
+    Friend WithEvents lblGrün As Label
+    Friend WithEvents btnWeiter As Button
 End Class
